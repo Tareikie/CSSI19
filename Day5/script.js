4440357e2a3595ef1eedaf2d77b59f1841ce2eaf
Tareikie <Oghenetareikie@mec.science>
2019-07-29 12:00:21 -0400

    script lessons

--- Day5/script.js
+++ Day5/script.js
@@ -8,3 +8,29 @@ console.log("7");
 console.log("8");
 console.log("9");
 console.log("10");
+
+// With one console.log() function
+console.log('1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
+
+//Prompt
+let result = prompt("Enter your name");
+console.log("Hello, ", result);
+
+let num = prompt("What is your favorite number");
+num = Number(num);
+
+if(isNaN(num))
+{
+    num = 10;
+}
+num = num + 10;
+console.log(num);
+// Task3
+
+let grade = prompt("What is your grade?");
+grade = Number(grade);
+if (grade < 70 && Number)
+    {console.log("You FAILED, stop playing fortnite and start studying!");}    
+else{
+    console.log("You Passed! great job!")
+}
---
+++ Day5/script2.js
@@ -0,0 +1,27 @@
+function f(x)
+{
+    return 3 * x + 7;
+}
+//Display the result of calling the
+//function f() with argument 2, 12, 7
+console.log(f(2));
+console.log(f(12));
+console.log(f(7));
+console.log("");
+
+function g(x)
+{
+    return 5 * x + 5;
+}
+console.log(g(2));
+console.log(g(12));
+console.log(g(7));
+console.log("");
+function i(x,y)
+{
+    return x**2-y**2;
+}
+console.log(i(3,4))
+console.log(i(4,3))
+console.log(i(5,3))
+console.log("")
